cff448f462d5f22f811c73a008341d60235ad149
Rocketshipclub <jaakko.kauranen@gmail.com>
2021-04-15 20:34:16 +0300

    Fix port definition for Heroku

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './config/app';
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
